1d2ddc6b041d06d2e78274d42027b257104e8260
Harry Sohal <meghna0593@gmail.com>
2024-05-20 15:49:30 -0700

    add dynamic lists

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,14 @@
 function ListGroup() {
+  const items = ["Vancouver", "Toronto", "Halifax", "Edmonton"];
+
   // go to getbootstrap.com -> docs -> list group
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">First</li>
-        <li className="list-group-item">Second</li>
-        <li className="list-group-item">Third</li>
-        <li className="list-group-item">Fourth</li>
-        <li className="list-group-item">Fifth</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
